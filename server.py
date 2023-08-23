c81362d898592360414a31158f729b71a3d91661
kushalvpalankar <kushalvpalankar@gmail.com>
2023-08-24 02:48:32 +0530

    hi

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def add():
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    return '0'
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
